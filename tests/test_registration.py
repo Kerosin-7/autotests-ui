73e863e94124611461c7fad6742d1ff709e00ebb
Kerosin-7 <llampo4ka@yandex.ru>
2026-08-22 23:31:06 +0300

    Фикстуры Pytest

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,27 +1,23 @@
-from playwright.sync_api import sync_playwright, expect
+import pytest
+from playwright.sync_api import expect, Page
 
 
-def test_successful_registration():
-    with sync_playwright() as playwright:
-        # Открываем браузер и создаем новую страницу
-        browser = playwright.chromium.launch(headless=False)
-        page = browser.new_page()
+@pytest.mark.regression
+@pytest.mark.registration
+def test_successful_registration(chromium_page: Page):  # Теперь используем фикстуру
+    chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
-        # Переходим на страницу
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    email_input = chromium_page.get_by_test_id('registration-form-email-input').locator('input')
+    email_input.fill('user.name@gmail.com')
 
-        # Заполняем поле email
-        email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-        email_input.fill("user.name@gmail.com")
+    username_input = chromium_page.get_by_test_id('registration-form-username-input').locator('input')
+    username_input.fill('username')
 
-        # Заполняем поле username
-        username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-        username_input.fill("username")
+    password_input = chromium_page.get_by_test_id('registration-form-password-input').locator('input')
+    password_input.fill('password')
 
-        # Заполняем поле пароль
-        password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-        password_input.fill("password")
+    registration_button = chromium_page.get_by_test_id('registration-page-registration-button')
+    registration_button.click()
 
-        # Нажимаем на кнопку registration
-        reg = page.get_by_test_id('registration-page-registration-button')
-        reg.click()
+    dashboard_title = chromium_page.get_by_test_id('dashboard-toolbar-title-text')
+    expect(dashboard_title).to_be_visible()
